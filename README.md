7abd608383dad3017ac68d76c726312dc3295472
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-03-21 14:56:39 -0300

    feat: Primeras pruebas escritas

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 [Clase 1 - Test y Mocks](https://docs.google.com/presentation/d/1D86TUECWKzFCGDcGUVH45wWNBrX7CCgcPtnUHxmwOgY/edit#slide=id.g120b44b0dae_0_1178)
 
-### TDD
+### Errores en entorno productivo
+
+| Tipo de error      | Descripción                                                                                                                                | Ejemplo                                                                                                            |
+| ------------------ | ------------------------------------------------------------------------------------------------------------------------------------------ | ------------------------------------------------------------------------------------------------------------------ |
+| **De compilación** | "Mi código no compila", ocurre cuando un código no puede iniciarse debido a que el compilador encontró algún error antes de ejecutarlo.    | Tema importaciones/exportaciones erroneas o que no existen                                                         |
+| **De ejecución**   | "Mi código explotó", ocurre cuando el código sí logra compilarse, pero a lo largo de su trabajo ocurre un error, usualmente no controlado. | Levanta, pero en alguna parte del camino, explota. Puedo ver los productos, pero si quiero ver el detalle explota. |
+| **Lógicos**        | "Funciona, pero no funciona", ocurre cuando el código compila y se ejecuta correctamente, pero el resultado no es el esperado.             | Agrego F al carrito, despues A, y al rato otro F, si figuran 3 items distintos es porque está mal                  |
+
+JS es un lenguaje INTERPRETADO, no compilado.
+
+### TDD - Test Driven Development | (Desarrollo Orientado a Pruebas)
+Es una metodología de desarrollo de software que consiste en pensar y escribir las pruebas que debe cumplir determinada funcion, incluso antes de escribirla.
+ Consiste en pasar por 3 etapas: 
+
+1. Escribir una prueba fallida
+
+2. Hacer que la prueba pase
+
+3. Refactorizar
+
+Ejemplo: Función Suma
+
+
+
+
+
+
+
 
 
 
 ### Desarrollo preventivo a partir de mocks
+
+00:43:00
---
+++ src/tdd.js
@@ -0,0 +1,72 @@
+// PASO 1 Escribir la funcion
+// const sumar = (a, b) => {
+//     return a + b;
+// };
+
+// Pensar múltiples escenarios para poner a prueba la función
+// 1. La función debe retornar null si algún parametro no es númerico
+// 2. La función debe retornar 0 si no se pasa ningún parametro
+// 3. La función debe poder realizar la suma correctamente.
+// 4. La función debe poder hacer la suma con cualquier cantidad de números.
+
+// PASO 2: Hacer que las pruebas pasen
+const sumar = (a, b) => {
+    return a + b;
+};
+
+
+// TEST 1
+const testTotales = 4;
+let testPasados = 0;
+console.log("1. La función debe retornar null si algún parametro no es númerico");
+
+const resultado1 = sumar("1", 2);
+if (resultado1 === null) {
+    console.log("✅ Test 1 pasó");
+    testPasados++;
+} else {
+    console.log("❌ Test 1 no pasó. Se esperaba null pero se recibió: ", resultado1);
+}
+
+console.log("--------------------------------------------------------------------------");
+
+// TEST 2
+console.log(" 2. La función debe retornar 0 si no se pasa ningún parametro");
+
+const resultado2 = sumar();
+if (resultado2 === 0) {
+    console.log("✅ Test 2 pasó");
+    testPasados++;
+} else {
+    console.log("❌ Test 2 no pasó. Se esperaba 0 pero se recibió: ", resultado2);
+}
+
+console.log("--------------------------------------------------------------------------");
+// TEST 3
+console.log("3. La función debe poder realizar la suma correctamente.");
+
+const resultado3 = sumar(1, 2);
+if (resultado3 === 3) {
+    console.log("✅ Test 3 pasó");
+    testPasados++;
+} else {
+    console.log(`❌ Test 3 no pasó. Se esperaba ${1 + 2} pero se recibió: ${resultado3}`);
+}
+
+console.log("--------------------------------------------------------------------------");
+// TEST 4
+console.log("La función debe poder hacer la suma con cualquier cantidad de números.");
+
+const resultado4 = sumar(1, 2, 4);
+if (resultado4 === 4) {
+    console.log("✅ Test 4 pasó");
+    testPasados++;
+} else {
+    console.log(`❌ Test 4 no pasó. Se esperaba ${1 + 2 + 4} pero se recibió: ${resultado4}`);
+}
+
+if (testPasados === testTotales) {
+    console.log("Felicitaciones, pasaste todos los tests!");
+} else{
+    console.log(`Se pasaron ${testPasados}/${testTotales} tests `);
+}
