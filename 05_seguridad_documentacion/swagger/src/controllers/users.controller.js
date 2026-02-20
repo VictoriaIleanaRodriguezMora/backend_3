a5fd964f2ace2606ebd75b5309eca961ffe7441c
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2026-02-20 09:04:55 -0300

    docs: responses.yml y request.yml

--- 05_seguridad_documentacion/swagger/src/controllers/users.controller.js
+++ 05_seguridad_documentacion/swagger/src/controllers/users.controller.js
@@ -1,37 +1,41 @@
 // users.controller.js
 
-import { usersService } from "../services/index.js"
+import { usersService } from "../services/index.js";
 
-const getAllUsers = async(req,res)=>{
-    const users = await usersService.getAll();
-    res.send({status:"success",payload:users})
-}
+const getAllUsers = async (req, res) => {
+  const users = await usersService.getAll();
+  res.send({ status: "success", payload: users });
+};
 
-const getUser = async(req,res)=> {
-    const userId = req.params.uid;
-    const user = await usersService.getUserById(userId);
-    if(!user) return res.status(404).send({status:"error",error:"User not found"})
-    res.send({status:"success",payload:user})
-}
+const getUser = async (req, res) => {
+  const userId = req.params.uid;
+  const user = await usersService.getUserById(userId);
+  if (!user)
+    return res.status(404).send({ status: "error", error: "User not found" }); // si acá en vez de 'error', dijera 'err', o 'msg', en el .yml de swagger, tmb debe decir exactamente el mismo nombre de prop
+  res.send({ status: "success", payload: user });
+};
 
-const updateUser =async(req,res)=>{
-    const updateBody = req.body;
-    const userId = req.params.uid;
-    const user = await usersService.getUserById(userId);
-    if(!user) return res.status(404).send({status:"error", error:"User not found"})
-    const result = await usersService.update(userId,updateBody);
-    res.send({status:"success",message:"User updated"})
-}
+const updateUser = async (req, res) => {
+  const updateBody = req.body;
+  const userId = req.params.uid;
+  const user = await usersService.getUserById(userId);
+  if (!user)
+    return res.status(404).send({ status: "error", error: "User not found" });
+  const result = await usersService.update(userId, updateBody);
+  res.send({ status: "success", message: "User updated" });
+};
 
-const deleteUser = async(req,res) =>{
-    const userId = req.params.uid;
-    const result = await usersService.getUserById(userId);
-    res.send({status:"success",message:"User deleted"})
-}
+const deleteUser = async (req, res) => {
+  const userId = req.params.uid;
+  const result = await usersService.getUserById(userId);
+  if(!result)
+    return 
+  res.send({ status: "success", message: "User deleted" });
+};
 
 export default {
-    deleteUser,
-    getAllUsers,
-    getUser,
-    updateUser
-}
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+};
